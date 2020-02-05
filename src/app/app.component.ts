ccc94ff2c3e57e091569801f37647dbcae3365e4
gurtejgill <gillgurtej96@gmail.com>
2020-02-05 22:03:23 +0000

    Create components dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  ComponentFactory
+} from '@angular/core';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title = 'app';
+  componentRef: any;
+  @ViewChild('loadComponent', { read: ViewContainerRef, static: true }) entry: ViewContainerRef;
+  constructor(private resolver: ComponentFactoryResolver) { }
+  createComponent(Id: number) {
+    this.entry.clear();
+    if (Id == 1) {
+      const factory = this.resolver.resolveComponentFactory(Component1Component);
+      this.componentRef = this.entry.createComponent(factory);
+    } else if (Id == 2) {
+      const factory = this.resolver.resolveComponentFactory(Component2Component);
+      this.componentRef = this.entry.createComponent(factory);
+    } 
+    this.componentRef.instance.message = "Called by appComponent";
+  }
+  destroyComponent() {
+    this.componentRef.destroy();
+  }
+  data = [
+    {
+      "Id": 1,
+      "Name": "Component 1"
+    },
+    {
+      "Id": 2,
+      "Name": "Component 2"
+    }
+  ]
+  selectName(id : number) {
+    this.createComponent(id);
+  }
 }
